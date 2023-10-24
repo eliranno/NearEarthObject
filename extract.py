2bc398238ab5fb83b68abb7c5c78c5bae6c4b0ba
Eliran Noach <eliranno@gmail.com>
2023-10-24 09:46:22 -0400

    Update extract.py

--- extract.py
+++ extract.py
@@ -26,7 +26,7 @@ def load_neos(neo_csv_path):
     :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
     :return: A collection of `NearEarthObject`s.
     """
-    # TODO: Load NEO data from the given CSV file.
+    
     record_list = []
     with open(neo_csv_path,'r') as csvfile:
         csvreader = csv.DictReader(csvfile)
@@ -46,7 +46,7 @@ def load_approaches(cad_json_path):
     :param cad_json_path: A path to a JSON file containing data about close approaches.
     :return: A collection of `CloseApproach`es.
     """
-    # TODO: Load close approach data from the given JSON file.
+    
     with open(cad_json_path,'r') as jsonfile:
         root_object = json.load(jsonfile)
         fields = root_object.get('fields')
